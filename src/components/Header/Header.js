c7c8112d3b0a6e6e29aa337ecfdd691ebd788b14
Jahir <engrmmjahir@gmail.com>
2022-10-08 01:12:00 +0600

    Dispaly TShirt and style t-shirt components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
         <nav>
             <Link to='/'>Home</Link>
             <Link to='/orders'>Orders</Link>
+            <Link to='/about'>About</Link>
         </nav>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import TShirt from '../TShirt/TShirt';
 import './Home.css';
 
 const Home = () => {
+    const tshirts = useLoaderData();
     return (
-        <div>
-            <h1>Home</h1>
+        <div className='home-container'>
+            <div className='tshirt-container'>
+                {
+                    tshirts.map(tshirt => <TShirt
+                        key={tshirt.id}
+                        tshirt={tshirt}
+                    ></TShirt>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
