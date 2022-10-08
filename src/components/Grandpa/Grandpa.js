b96edb5b412a192477f0fcab0859ef6e3eb9b349
Jahir <engrmmjahir@gmail.com>
2022-10-08 12:58:39 +0600

    Prop Drilling to pass data to a many level components

--- src/components/Grandpa/Grandpa.js
+++ src/components/Grandpa/Grandpa.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Aunty from '../Aunty/Aunty';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import './Grandpa.css';
 
+const RingContext = createContext('Golden Ring');
+
 const Grandpa = () => {
     const house = 7;
+    const ring = 'Diamond Ring';
     return (
         <div className='grandpa'>
             <h2>Grandpa</h2>
             <p><small>House: {house}</small></p>
             <section className='flex'>
-                <Father house={house}></Father>
+                <Father house={house} ring={ring}></Father>
                 <Uncle house={house}></Uncle>
                 <Aunty house={house}></Aunty>
             </section>
